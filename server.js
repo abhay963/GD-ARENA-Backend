57cb8b82aa4ab267c6644ee79356439998991add
abhay963 <abhayyadav96312@gmail.com>
2025-12-25 19:01:55 +0530

    minor fix

--- server.js
+++ server.js
@@ -456,7 +456,7 @@ User just said:
 ${userSpeech}
 
 Rules:
-- 6-10 lines max
+- 15-20 lines max
 - Maintain character consistency
 - Player 1: aggressive, challenges viewpoints
 - Player 2: analytical, provides balanced perspective
